b116808233392ef2f836c0b1eb1a8a196f33fa22
rafaelapessin <rafaelapessin@outlook.com>
2025-12-14 12:56:58 -0300

    Ajustes

--- bin/www.js
+++ bin/www.js
@@ -1,16 +1,18 @@
 const app = require('../app');
-
 const porta = 3000;
 
-app.listen(porta, () => {
+app.listen(porta, () => 
+{
   console.log(`Servidor rodando em http://localhost:${porta}`);
 });
 
 // captura qualquer erro fatal
-process.on('uncaughtException', err => {
+process.on('uncaughtException', err => 
+{
   console.error('ERRO NÃO TRATADO:', err);
 });
 
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', err => 
+{
   console.error('PROMISE NÃO TRATADA:', err);
 });
