a538238d6991aec74890365d0013b32f3be4d7ff
rafaelapessin <rafaelapessin@outlook.com>
2025-12-14 13:08:27 -0300

    Ajustes

--- controller/produtoController.js
+++ controller/produtoController.js
@@ -1,13 +1,28 @@
 const Produto = require('../model/mongoose/Produto');
 
-exports.listarProdutos = async (req, res) => 
-{
-    const produtos = await Produto.find({}, 'nome preco estoque').limit(8);
-    res.render('home', { produtos });
+exports.listarProdutos = async (req, res) => {
+  try {
+    const produtos = await Produto
+      .find({}, 'nome preco estoque')
+      .limit(8)
+      .lean(); // 👈 transforma em objeto JS puro
+
+    res.render('dashboard', { produtos });
+  } catch (erro) {
+    console.error('Erro ao listar produtos:', erro);
+    res.status(500).send('Erro ao carregar produtos');
+  }
 };
 
-exports.detalhesProduto = async (req, res) => 
-{
-    const produto = await Produto.findById(req.params.id);
+exports.detalhesProduto = async (req, res) => {
+  try {
+    const produto = await Produto
+      .findById(req.params.id)
+      .lean(); // 👈 MUITO IMPORTANTE
+
     res.render('detalhesProduto', { produto });
+  } catch (erro) {
+    console.error('Erro ao buscar produto:', erro);
+    res.status(500).send('Erro ao carregar detalhes do produto');
+  }
 };
